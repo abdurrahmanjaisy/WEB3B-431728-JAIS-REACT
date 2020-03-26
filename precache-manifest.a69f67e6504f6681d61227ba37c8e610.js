9c729ba94de9eceef4fe25511d46a31d523620f1
abdurrahmanjaisy <abdurrahmanjaisy@mail.ugm.ac.id>
2020-03-26 20:58:00 +0700

    Updates

---
+++ precache-manifest.a69f67e6504f6681d61227ba37c8e610.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37c99c2745bd569cb3a8a2b75dd6b3b",
+    "url": "/WEB3B-431728-JAIS-REACT/index.html"
+  },
+  {
+    "revision": "29979a20006a2feefe52",
+    "url": "/WEB3B-431728-JAIS-REACT/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "4e65b9df3ced24cd9dca",
+    "url": "/WEB3B-431728-JAIS-REACT/static/css/main.7259bae9.chunk.css"
+  },
+  {
+    "revision": "29979a20006a2feefe52",
+    "url": "/WEB3B-431728-JAIS-REACT/static/js/2.2a154198.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/WEB3B-431728-JAIS-REACT/static/js/2.2a154198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e65b9df3ced24cd9dca",
+    "url": "/WEB3B-431728-JAIS-REACT/static/js/main.1562d371.chunk.js"
+  },
+  {
+    "revision": "d1e6996276a687825be0",
+    "url": "/WEB3B-431728-JAIS-REACT/static/js/runtime-main.68689d37.js"
+  },
+  {
+    "revision": "23f06eaaca7b5cf57f6b188afa9ac91c",
+    "url": "/WEB3B-431728-JAIS-REACT/static/media/panenSayur.23f06eaa.jpg"
+  },
+  {
+    "revision": "ff7d11c8acfe9a954e55985a6925e004",
+    "url": "/WEB3B-431728-JAIS-REACT/static/media/picture.ff7d11c8.svg"
+  },
+  {
+    "revision": "b20198a8d29c99e0ffbe287a6e03ee1f",
+    "url": "/WEB3B-431728-JAIS-REACT/static/media/ternakAyam.b20198a8.jpg"
+  },
+  {
+    "revision": "b1a13f08f6d3d0ffaf229c1ebc7be37b",
+    "url": "/WEB3B-431728-JAIS-REACT/static/media/ternakSapi.b1a13f08.jpg"
+  }
+]);
